7e8f9c5464dd9ed6acbcf86fcb62ec3b152e60fd
Jason Nguyen <jasonvex@gmail.com>
2016-11-24 22:30:06 -0600

    Implememented cellForItemAtIndexPath method for UICollectionView. Took forever to figure out that I was not implementing numberOfItemsInSection and was only implementing numberOfSections

--- YouTube-Swift/ViewController.swift
+++ YouTube-Swift/ViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class HomeController: UICollectionViewController {
 
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Home"
+        
         collectionView?.backgroundColor = UIColor.white
+        
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
 }
 
