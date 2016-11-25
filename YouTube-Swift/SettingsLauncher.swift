c765246f62033c32eb1124aa9f5a9ab53e7bbdcc
Jason Nguyen <jasonvex@gmail.com>
2016-11-25 05:47:28 -0600

    Implement animation for Slide-In Menu Ep7

--- YouTube-Swift/SettingsLauncher.swift
+++ YouTube-Swift/SettingsLauncher.swift
@@ -12,8 +12,14 @@ class SettingsLauncher: NSObject{
     
     let blackView = UIView()
     
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        return cv
+    }()
+    
     func showSettings() {
-        //show menu
         
         if let window = UIApplication.shared.keyWindow {
             
@@ -22,12 +28,22 @@ class SettingsLauncher: NSObject{
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             
             window.addSubview(blackView)
+            
+            window.addSubview(collectionView)
+            
+            let height: CGFloat = 200
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             blackView.frame = window.frame
             blackView.alpha = 0
             
-            UIView.animate(withDuration: 0.5, animations: {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
-            })
+                
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }, completion: nil)
+            
         }
         
     }
@@ -35,6 +51,10 @@ class SettingsLauncher: NSObject{
     func handleDismiss() {
         UIView.animate(withDuration: 0.5, animations: {
             self.blackView.alpha = 0
+            
+            if let window = UIApplication.shared.keyWindow {
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
         })
     }
     
