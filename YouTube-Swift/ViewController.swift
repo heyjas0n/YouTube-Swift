496b7fe5399ff5242b11ae18a451a14138b2d081
Jason Nguyen <jasonvex@gmail.com>
2016-11-24 21:58:40 -0600

    Initial Commit

---
+++ YouTube-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTube-Swift
+//
+//  Created by Nguyen, Jason on 11/24/16.
+//  Copyright © 2016 Jason Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
